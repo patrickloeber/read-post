396f64a2cb3486b00401820f7102defa72dad9e5
Patrick Loeber <patloeber@google.com>
2026-05-05 04:06:28 +0900

    clean up

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,19 @@ import { Webhook } from "standardwebhooks";
 
 export default {
 	async fetch(request, env): Promise<Response> {
-		console.log(`Incoming request: ${request.method} ${request.url}`);
 
 		// Handle POST requests
 		if (request.method === "POST") {
 			const payload = await request.text();
-			console.log("Payload:", payload);
 			const headers: Record<string, string> = {};
 			request.headers.forEach((value, key) => {
 				headers[key] = value;
 			});
 
-			console.log("Headers:", JSON.stringify(headers));
-			console.log("Secret: ", env.WEBHOOK_SIGNING_SECRET);
-
 			try {
-				const wh = new Webhook(env.WEBHOOK_SIGNING_SECRET, { format: "raw" });
+				const wh = new Webhook(env.WEBHOOK_SIGNING_SECRET);
 				const event = wh.verify(payload, headers) as Record<string, any>;
 
-				// Process webhook event
-				console.log("Verified event:", JSON.stringify(event));
 				if (event.type === "batch.completed" || event.type === "video.generated") {
 					const uri = event.data.output_file_uri;
 					console.log(`Job finished! Results at: ${uri}`);
